52b5bf82c6924d4485c0dc5dcf9718f62735cc17
Eric Fuemmeler <101132913+EFuem@users.noreply.github.com>
2024-01-16 17:05:32 -0600

    fixed requests

--- cli/utils.py
+++ cli/utils.py
@@ -3,7 +3,6 @@ from pprint import pprint
 import json
 import requests
 def _query(text=None, elements=None, elements_exact=None, properties=None):
-    #ds_client = MongoClient("mongodb://CLI:CLI@localhost:27017")['cf-update-2023-11-30']['datasets']
     query = {}
     if text is not None:
         query['$text']={'$search':text}
@@ -22,17 +21,15 @@ def _query(text=None, elements=None, elements_exact=None, properties=None):
 
     #post to REST API
     q = requests.post('https://cf.hsrn.nyu.edu/datasets',json=query) 
-    #q = ds_client.find(query,{'name':1,'colabfit-id':1,'authors':1,'links':1,'aggregated_info.elements':1,'aggregated_info.property_types':1,'aggregated_info.nconfigurations':1, 'description':1})
-    return q
+    return q.json()
     
 def format_print(doc):
     new_doc={}
-    doc.pop('_id')
     new_doc['colabfit-id']=doc['colabfit-id']
     new_doc['name']=doc['name']
     new_doc['authors']=doc['authors']
     new_doc['description']=doc['description']
     new_doc['links']=doc['links']
     new_doc['links'].append('https://materials.colabfit.org/id/%s'%doc['colabfit-id'])
-    new_doc['aggregated_info']=doc['aggregated_info']
+    #new_doc['aggregated_info']=doc['aggregated_info']
     pprint (new_doc,sort_dicts=False)
