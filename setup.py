e26c74f18cc552cf1ea625ed43b40a6ba9fc72ee
Eric Fuemmeler <101132913+EFuem@users.noreply.github.com>
2024-01-17 20:19:39 -0600

    added tqdm

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     url="https://github.com/colabfit/colabfit-cli",
     packages=setuptools.find_packages(),
     install_requires=[
-         'click>=7.0', 'requests',
+         'click>=7.0', 'requests', 'tqdm',
     ],
     python_requires='>=3.7',
     entry_points="""
